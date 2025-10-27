bcdd350878fcf49d7e684773289cbc518b6d8369
it21258862 <99663609+IT21316722@users.noreply.github.com>
2025-10-27 20:39:07 +0530

    Github pages

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.pptx', '**/*.pdf'], // allow importing pptx/pdf
-});
